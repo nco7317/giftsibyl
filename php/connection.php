c1dbdc6a6a54a02cca38cc2ba4f92f1a7e3d3e6f
nco7317 <nicholasnwadei@gmail.com>
2020-11-04 21:51:17 -0600

    initial commit

---
+++ php/connection.php
@@ -0,0 +1,8 @@
+<?php
+    $host = "localhost";
+    $dbUsername = "giftsiby_admin";
+    $dbPassword = "Carbar0228!";
+    $dbname = "giftsiby_gifts";
+$conn = mysqli_connect($host, $dbUsername, $dbPassword, $dbname);
+
+?>
